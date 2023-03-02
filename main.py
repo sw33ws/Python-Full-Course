ccf9aa9d4d7c2d512fb5f26afa498927fb83b556
sw33ws <gillisl22@outlook.com>
2023-03-02 13:59:49 -0500

    messagebox

--- main.py
+++ main.py
@@ -1392,4 +1392,58 @@
 
 
 
-# Messagebox
+# # Messagebox
+# from tkinter import *
+# from tkinter import messagebox
+
+# def click():
+#     # messagebox.showinfo(title='This is an info message box',message='You are a person')
+
+#     # while(True):
+#         # messagebox.showwarning(title='Warning',message='You have a VIRUS!!!')             # This is how to make an uncloseable popup
+#     # messagebox.showerror(title='ERROR!',message='Something went wrong :(')
+
+    
+#     # if messagebox.askokcancel(title='ask of cancel',message='Do you want to do the thing'):
+#     #     print('You did a thing')
+#     # else:
+#     #     print('You canceled a thing! :(')
+
+#     # if messagebox.askretrycancel(title='ask of cancel',message='Do you want retry the thing'):
+#     #     print('You retried a thing')
+#     # else:
+#     #     print('You canceled a thing! :(')
+
+#     # if messagebox.askyesno(title='ask yes or no',message='Do you like cake?'):
+#     #     print('I like cake too')
+#     # else:
+#     #     print('Why do you not like cake')
+
+#     # answer = messagebox.askquestion(title='ask question',message='Do you like pie?')
+#     # if(answer == 'yes'):
+#     #     print('I like pie too')
+#     # else:
+#     #     print('Why do you not like pie too')
+
+#     answer = messagebox.askyesnocancel(title=' Yes no cancel',message='Do you like to code?',icon='error') # icon lets you set the icon
+#     if(answer==True):
+#         print("You like to code")
+#     elif(answer==False):
+#         print("Then why are you coding")
+#     else:
+#         print("You have dodged the question")
+
+# window = Tk()
+# # window.geometry("420x420") 
+# window.title("Messagebox") 
+
+# button = Button(window,
+#                 command=click,
+#                 text='click me')
+# button.pack()
+
+# window.mainloop()
+
+
+
+# 
