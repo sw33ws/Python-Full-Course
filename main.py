15b896a8871ac407cbe51cf4d2df1ec55ba959b7
sw33ws <gillisl22@outlook.com>
2023-03-04 23:59:35 -0500

    progress bar

--- main.py
+++ main.py
@@ -1689,6 +1689,42 @@
 
 
 
+# # Progress Bar
+# from tkinter import *
+# from tkinter.ttk import *
+# import time
+
+# def start():
+#     GB = 100
+#     download = 0
+#     speed = 1
+#     while(download<GB):
+#         time.sleep(0.05)
+#         bar['value']+=(speed/GB)*100
+#         download+=speed
+#         percent.set(str(int((download/GB)+100))+"%")
+#         text.set(str(download)+"/"+str(GB)+" GB completed")
+#         window.update_idletasks
+
+# window = Tk()
+# # window.geometry("420x420") 
+# window.title("Progress Bar") 
+
+# percent = StringVar()
+# text = StringVar()
+
+# bar = Progressbar(window,orient=HORIZONTAL,length=300)
+# bar.pack(pady=10)
+
+# percentLabel = Label(window,textvariable=percent).pack()
+# taskLabel = Label(window,textvariable=text).pack()
+
+# button = Button(window,text="download",command=start).pack()
+
+# window.mainloop()
+
+
+
 #
 # from tkinter import *
 
