4b55056a625e6065bb01d7bd4bda6dfd38e5e54c
sw33ws <gillisl22@outlook.com>
2023-03-06 14:20:19 -0500

    canvas

--- main.py
+++ main.py
@@ -1725,7 +1725,46 @@
 
 
 
-#
+# # Canvas
+# from tkinter import *
+
+# window = Tk()
+# # window.geometry("420x420") 
+# window.title("New Windows") 
+
+# canvas = Canvas(window,height=500,width=500)
+# # canvas.create_line(0,0,500,500,fill="blue",width=5)
+# # canvas.create_line(0,500,500,0,fill="red",width=5)
+# # canvas.create_rectangle(50,50,250,250,fill="purple")
+
+# # points = [250,0,500,500,0,500]
+# # canvas.create_polygon(points,fill="yellow",outline="black",width=5)
+
+# # canvas.create_arc(0,0,500,500,fill="green",style=PIESLICE,start=0,extent=180)
+
+# canvas.create_arc(0,0,500,500,fill="red",extent=180,width=10)
+# canvas.create_arc(0,0,500,500,fill="white",extent=180,start=180,width=10)
+# canvas.create_oval(190,190,310,310,fill="white",width=10)
+# canvas.create_oval(220,220,280,280,width=1)
+
+# canvas.pack()
+
+# window.mainloop()
+
+
+
+# Keyboard Events
+from tkinter import *
+
+window = Tk()
+# window.geometry("420x420") 
+window.title("New Windows") 
+
+window.mainloop()
+
+
+
+# 
 # from tkinter import *
 
 # window = Tk()
